42fea0b4ede66ef4f5c12678a1eab0f1d4d267f2
Sujith K <sujithk1910@gmail.com>
2021-04-19 19:22:35 +0530

    added basic optimizer code

--- Optimization.py
+++ Optimization.py
@@ -1,4 +1,34 @@
-# symbol_table = open('./Token Generator/symbol_table.txt',"r+")
-# icg = open('./ICG/icg.txt',"r+")
-# for line in symbol_table:
-#     print(line.split())
+symbol_table=open('symbol_table.txt','r+')
+icg=open('icg.txt','r+')
+
+
+def print_optimized_icg():
+    for id in st_dict:
+        print(id + ' = ' + st_dict[id][0])
+st_dict=dict()
+i=0
+for line in symbol_table:
+    line=line.split()
+    if len(line)==0 or line[0]=='Name' or line[0][0]=='-':
+        pass
+    else:
+        identifier=line[0]
+        if identifier not in st_dict:
+            st_dict[identifier]=[]
+            st_dict[identifier].append(line[3])
+            st_dict[identifier].append(line[-1]) 
+
+        else:
+            if st_dict[identifier][-1]==line[-1]:
+                st_dict[identifier]=[]
+                st_dict[identifier].append(line[3])
+                st_dict[identifier].append(line[-1])
+            else:
+                # print_optimized_icg()
+                pass
+
+        # print(line)
+
+# print(st_dict)
+print_optimized_icg()
+

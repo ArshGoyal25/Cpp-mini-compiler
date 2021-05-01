e8b35f676acfb73ebcf587da1d4627d40f16771b
Arsh Goyal <arshgoyal@gmail.com>
2021-05-01 19:10:22 +0530

    Added error input file

--- Input_Files/input_file_err.cpp
+++ Input_Files/input_file_err.cpp
@@ -1,18 +1,23 @@
 #include<iostream>
 int main()
 {
-    int a = 5, b = 4, c = 2;
+    int abcd123456789abcd123456789efghijklm;
+    int a = 5, b = 4, c = 2
     b = b*4;
-    int d = 2;
+    int d = 2.4;
     float e = a * b / c + d;
+    m = 10
+    float s = "hello";
     while(b<10){
         a = b-1;
     }
+    break;
     float z = 4.2;
     if(z > 4)
         b = 100;
     else
         b = 200;
+    int a = 20;
 	switch(d){
         case 1 : d = a + b ;
             break;
